0b1a039e91f1edd86300c095f6950652938a81bb
virgo <virgostevanus27@gmail.com>
2025-01-27 14:49:44 +0700

    add welcome text at admin dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,5 +1,11 @@
 @extends('admin.layouts.app')
 
 @section('content')
-   
+    {{-- Welcome admin page --}}
+   <div class="row justify-content-center">
+        {{-- welcome text --}}
+        <div class="col-md-12">
+            <h1>Welcome to the Admin Dashboard</h1>
+        </div>  
+   </div>
 @endsection
